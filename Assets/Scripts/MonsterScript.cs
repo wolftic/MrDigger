f40a6e407eaf5c464eb6901ad9134f0b4da38609
casperheringa <5be8141b@opayq.com>
2016-09-19 10:54:41 +0200

    movement and block destoy

---
+++ Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterScript : MonoBehaviour {
+
+	
+	void Update () {
+        transform.Translate(0, 0.017f, 0);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Blocks"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }   
+}
---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public float speed;
+
+    
+
+    void Update()
+    {
+        transform.Translate((-Vector2.up * 0.5f + Vector2.right * Input.GetAxis("Horizontal")) * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(0, 0, 2);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, -5);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Blocks")) {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
