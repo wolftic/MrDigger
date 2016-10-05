639ae7ab5e62d9bc42fbfa2fbdc5eb5cb7a776a7
casperheringa <5be8141b@opayq.com>
2016-10-05 17:16:32 +0200

    menu

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class UIScript : MonoBehaviour
+{
+    public Button butt;
+    public GameObject monster;
+    private Vector3 goTo;
+    private bool goToPlace = false;
+
+    void Start()
+    {
+        Button btn = butt.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void Update() {
+        if (goToPlace) {
+            Camera.main.transform.localPosition = Vector3.Lerp(Camera.main.transform.localPosition, goTo, 3f * Time.deltaTime);
+            gameObject.GetComponent<Text>().enabled = false;
+        }
+       
+    }
+
+    void TaskOnClick() {
+       goTo  = new Vector3(0, -4.26f, -13.222f);
+        goToPlace = true;
+    }
+}
